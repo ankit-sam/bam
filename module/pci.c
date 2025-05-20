f259c37275dd48e9ed108c6709cae679295189ac
Ankit Kumar <ankit.kumar@samsung.com>
2025-05-19 22:02:03 -0700

    module: newer kernel changes
    get_user_pages() modified since 6.5 kernel
    class_create() modifedsince 6.4 kernel
    
    Note: These changes are only tested with 6.8 kernel
    
    Signed-off-by: Ankit Kumar <ankit.kumar@samsung.com>

--- module/pci.c
+++ module/pci.c
@@ -2,6 +2,7 @@
 #include "list.h"
 #include "ctrl.h"
 #include "map.h"
+#include <linux/version.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/kernel.h>
@@ -328,7 +329,12 @@ static int __init libnvm_helper_entry(void)
     }
 
     // Create character device class
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(6, 3, 13)
+#warning "Building for older kernel, not properly tested"
     dev_class = class_create(THIS_MODULE, DRIVER_NAME);
+#else
+    dev_class = class_create(DRIVER_NAME);
+#endif
     if (IS_ERR(dev_class))
     {
         unregister_chrdev_region(dev_first, max_num_ctrls);
